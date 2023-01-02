7553650026ffb9ce635e7b44699f9fd2856d0731
Bonce <chris@headstrong.solutions>
2023-01-02 15:04:22 +0000

    Added PCF8591 implementation
    No idea if this will actually work, yet :D

--- code.py
+++ code.py
@@ -11,7 +11,8 @@
 import usb_hid
 import board
 from time import time
-from analogio import AnalogIn
+import adafruit_pcf8591.pcf8591 as PCF
+from adafruit_pcf8591.analog_in import AnalogIn
 from adafruit_circuitplayground.express import cpx
 from adafruit_hid.mouse import Mouse
 from adafruit_hid.keyboard import Keyboard
@@ -38,18 +39,27 @@ RX = 3
 RY = 4
 RZ = 5
 
-# TODO - These need to change when the A2D SPI boards arrive 
-STICK1_VRX_PIN = AnalogIn(board.D0)
-STICK1_VRY_PIN = AnalogIn(board.D1)
-STICK1_SWI_PIN = AnalogIn(board.D3)
+# board.SCL and board.SDA on I2C0
+# no numbered pin definition is required as long 
+# as I2C devices are on I2C0 pins
+i2c = board.I2C()  
+pcf1 = PCF.PCF8591(i2c, address=0x048, reference_voltage=3.3)
+pcf2 = PCF.PCF8591(i2c, address=0x049, reference_voltage=3.3)
 
-STICK2_VRX_PIN = AnalogIn(board.D4)
-STICK2_VRY_PIN = AnalogIn(board.D5)
-STICK2_SWI_PIN = AnalogIn(board.D6)
+STICK1_VRX_PIN = AnalogIn(pcf1, PCF.A0)
+STICK1_VRY_PIN = AnalogIn(pcf1, PCF.A1)
+# currently unused
+# STICK1_SWI_PIN = AnalogIn(board.D3)
 
-STICK3_VRX_PIN = AnalogIn(board.D7)
-STICK3_VRY_PIN = AnalogIn(board.D8)
-STICK3_SWI_PIN = AnalogIn(board.D9)
+STICK2_VRX_PIN = AnalogIn(pcf1, PCF.A2)
+STICK2_VRY_PIN = AnalogIn(pcf1, PCF.A3)
+# currently unused
+# STICK2_SWI_PIN = AnalogIn(board.D6)
+
+STICK3_VRX_PIN = AnalogIn(pcf2, PCF.A0)
+STICK3_VRY_PIN = AnalogIn(pcf2, PCF.A1)
+# currently unused
+# STICK3_SWI_PIN = AnalogIn(board.D9)
 
 ANALOGUE_PINS = [STICK1_VRX_PIN, STICK2_VRX_PIN, STICK3_VRX_PIN, 
         STICK1_VRY_PIN, STICK2_VRY_PIN, STICK3_VRY_PIN]  
